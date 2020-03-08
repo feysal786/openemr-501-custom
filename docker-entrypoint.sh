f9d7de81c31dc3ea2c072a1ca157d3bc755e4ca1
Kamran Azeem <kaz@praqma.net>
2020-03-08 17:30:20 +0100

    added exec instruction at the end of docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,3 +5,6 @@ sed -i -e "s/SQLHOST/$SQLHOST/g" \
        -e "s/LOGIN/$LOGIN/g" \
        -e "s/PASSWORD/$PASSWORD/g" \
        /var/www/localhost/htdocs/sites/default/sqlconf.php
+
+# Execute the command specified as CMD in Dockerfile:
+exec "$@"
